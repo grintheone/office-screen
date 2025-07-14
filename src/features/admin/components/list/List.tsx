1ffbb7c7eb75b9965bfbda33ad5d6161b6bdb7ab
grintheone <grin3run@gmail.com>
2025-07-14 09:10:46 +0300

    fix: add conditional display of cards in admin

--- src/features/admin/components/list/List.tsx
+++ src/features/admin/components/list/List.tsx
@@ -6,21 +6,41 @@ import {
     type AdminContentTypes,
     setModalType,
 } from "@/features/admin/adminSlice";
+import BirthdayCard from "@/features/admin/components/cards/BirthdayCard";
+import ClockCard from "@/features/admin/components/cards/ClockCard";
+import ExtraCard from "@/features/admin/components/cards/ExtraCard";
+import HolidayCard from "@/features/admin/components/cards/HolidayCard";
+import QuoteCard from "@/features/admin/components/cards/QuoteCard";
 
 export interface IList {
     type: AdminContentTypes;
     name: string;
-};
+}
 
 // temporary hack to scale the panels
 // and allow scrolling within
 const height = "calc(100vh - 222px)";
 
+function displayCardByType(type: AdminContentTypes) {
+    switch (type) {
+        case "birthday":
+            return <BirthdayCard />;
+        case "holiday":
+            return <HolidayCard />;
+        case "extra":
+            return <ExtraCard />;
+        case "quote":
+            return <QuoteCard />;
+        case "clock":
+            return <ClockCard />
+    }
+}
+
 function List({ type, name }: IList) {
     const dispatch = useAppDispatch();
 
     return (
-        <section className="flex flex-col rounded-md ring-2 ring-primary/5">
+        <section className="flex flex-col rounded-md ring-2 ring-primary/10">
             <div className="text-lg flex justify-between items-center p-4">
                 {name}{" "}
                 <DialogTrigger asChild>
@@ -33,9 +53,11 @@ function List({ type, name }: IList) {
                 </DialogTrigger>
             </div>
             <ScrollArea
-                className="rounded-md border bg-primary/3 p-4 inset-shadow-sm inset-shadow-primary/10 m-2"
+                className="rounded-md border bg-primary/3 inset-shadow-sm inset-shadow-primary/10 m-2 p-0"
                 style={{ maxHeight: height, minHeight: height }}
-            ></ScrollArea>
+            >
+                {displayCardByType(type)}
+            </ScrollArea>
         </section>
     );
 }
