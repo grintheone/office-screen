e48490cda6a491e3c459ac4286c61080a553522c
grintheone <grin3run@gmail.com>
2025-07-16 12:33:02 +0300

    fix: fix index imports

--- src/features/display/index.tsx
+++ src/features/display/index.tsx
@@ -1,7 +1,7 @@
 import BackgroundWrapper from "@/features/display/components/background-wrapper/BackgroundWrapper";
 import ClockFeed from "@/features/display/components/clock-feed/ClockFeed";
 import MainFeed from "@/features/display/components/main-feed/MainFeed";
-import Marquee from "@/features/marquee/Marquee";
+import Marquee from "@/features/display/components/marquee/Marquee";
 
 function Display() {
     return (
