19c4bec276eae4997863adee499fd97f3916788c
grintheone <grin3run@gmail.com>
2025-07-25 14:25:42 +0300

    feat: add effect to info/holiday cards

--- src/features/display/components/cards/HolidayCard.tsx
+++ src/features/display/components/cards/HolidayCard.tsx
@@ -1,13 +1,36 @@
-import image from "@/assets/images/test3.jpeg"
-import type { HolidayDocument } from "@/services/AdminService"
+import { useEffect } from "react";
+import { useAppDispatch } from "@/app/hooks";
+import image from "@/assets/images/test3.jpeg";
+import { setEffect } from "@/features/display/displaySlice";
+import type { HolidayDocument } from "@/services/AdminService";
 
 function HolidayCard(doc: HolidayDocument) {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (doc.effect === "none") {
+            return;
+        }
+
+        const id = setTimeout(() => {
+            dispatch(setEffect(doc.effect));
+        }, 2000);
+
+        return () => clearTimeout(id);
+    }, [dispatch, doc.effect]);
+
     return (
         <div className="flex flex-col gap-8 max-w-3xl animate-rotate-y">
-            <img className="max-h-[600px] size-full object-left object-contain rounded-xl" src={image} alt="holiday" />
-            <div className="text-4xl text-white animate-slide-up opacity-0">{doc.title}</div>
+            <img
+                className="max-h-[600px] size-full object-left object-contain rounded-xl"
+                src={image}
+                alt="holiday"
+            />
+            <div className="text-4xl text-white animate-slide-up opacity-0">
+                {doc.title}
+            </div>
         </div>
-    )
+    );
 }
 
-export default HolidayCard
+export default HolidayCard;
--- src/features/display/components/cards/InfoCard.tsx
+++ src/features/display/components/cards/InfoCard.tsx
@@ -1,17 +1,44 @@
-import image from "@/assets/images/equip3.png"
-import type { InfoDocument } from "@/services/AdminService"
+import { useEffect } from "react";
+import { useAppDispatch } from "@/app/hooks";
+import image from "@/assets/images/equip3.png";
+import { setEffect } from "@/features/display/displaySlice";
+import type { InfoDocument } from "@/services/AdminService";
 
 function InfoCard(doc: InfoDocument) {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (doc.effect === "none") {
+            return;
+        }
+
+        const id = setTimeout(() => {
+            dispatch(setEffect(doc.effect));
+        }, 2000);
+
+        return () => clearTimeout(id);
+    }, [dispatch, doc.effect]);
+
     return (
         <div className="flex flex-col gap-8 max-w-3xl animate-rotate-y">
             <div className="text-3xl text-white animate-slide-up opacity-0 whitespace-pre-wrap">
                 {doc.text}
             </div>
-            {/* Если нет текста */}
-            {/* <img className="max-h-[600px] size-full object-left object-contain rounded-xl" src={image} alt="image" />  */}
-            <img className="max-h-[500px] size-full object-left object-contain rounded-xl" src={image} alt="card" />
+            {!doc.text && doc.text.length === 0 ? (
+                <img
+                    className="max-h-[600px] size-full object-left object-contain rounded-xl"
+                    src={image}
+                    alt="card"
+                />
+            ) : (
+                <img
+                    className="max-h-[500px] size-full object-left object-contain rounded-xl"
+                    src={image}
+                    alt="card"
+                />
+            )}
         </div>
-    )
+    );
 }
 
-export default InfoCard
+export default InfoCard;
