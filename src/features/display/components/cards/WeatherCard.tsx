58bb2b31440e8bf1b400e53eee20409ce47709c5
grintheone <grin3run@gmail.com>
2025-10-06 16:52:09 +0700

    feat: add new weather card videos for lis and kpd

--- src/features/display/components/cards/WeatherCard.tsx
+++ src/features/display/components/cards/WeatherCard.tsx
@@ -1,18 +1,28 @@
-import { useAppDispatch, useAppSelector } from "@/app/hooks"
-import solt_clear from "@/assets/videos/solt_clear.mp4"
-import solt_cloudy from "@/assets/videos/solt_cloudly.mp4"
-import solt_few_clouds from "@/assets/videos/solt_few_clouds.mp4"
-import solt_rain from "@/assets/videos/solt_rain.mp4"
-import solt_thunder from "@/assets/videos/solt_thunder.mp4"
-import vbb_clear from "@/assets/videos/vbb_clear.mp4"
-import vbb_cloudy from "@/assets/videos/vbb_cloudly.mp4"
-import vbb_few_clouds from "@/assets/videos/vbb_few_clouds.mp4"
-import vbb_rain from "@/assets/videos/vbb_rain.mp4"
-import vbb_thunder from "@/assets/videos/vbb_thunder.mp4"
-import { setVideoSlideState } from "@/features/display/displaySlice"
-import { selectTheme } from "@/features/settings/settingsSlice"
-
-import type { ParserDataItem } from "@/hooks/useParserData"
+import { useEffect, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import kpd_clear from "@/assets/videos/kpd_weather/clear.jpg";
+import kpd_few_clouds from "@/assets/videos/kpd_weather/few_clouds.webm"
+import kpd_heavy_clouds from "@/assets/videos/kpd_weather/heavy_clouds.webm"
+import kpd_heavy_rain from "@/assets/videos/kpd_weather/heavy_rain.webm"
+import kpd_rain from "@/assets/videos/kpd_weather/rain.webm"
+import lis_clear from "@/assets/videos/lis_weather/clear.jpg"
+import lis_few_clouds from "@/assets/videos/lis_weather/few_clouds.webm"
+import lis_heavy_clouds from "@/assets/videos/lis_weather/heavy clouds.webm"
+import lis_rain from "@/assets/videos/lis_weather/rain.webm"
+import lis_thunder from "@/assets/videos/lis_weather/thunder.webm"
+import solt_clear from "@/assets/videos/solt_clear.mp4";
+import solt_cloudy from "@/assets/videos/solt_cloudly.mp4";
+import solt_few_clouds from "@/assets/videos/solt_few_clouds.mp4";
+import solt_rain from "@/assets/videos/solt_rain.mp4";
+import solt_thunder from "@/assets/videos/solt_thunder.mp4";
+import vbb_clear from "@/assets/videos/vbb_clear.mp4";
+import vbb_cloudy from "@/assets/videos/vbb_cloudly.mp4";
+import vbb_few_clouds from "@/assets/videos/vbb_few_clouds.mp4";
+import vbb_rain from "@/assets/videos/vbb_rain.mp4";
+import vbb_thunder from "@/assets/videos/vbb_thunder.mp4";
+import { selectVideoSlideState, setVideoSlideState } from "@/features/display/displaySlice";
+import { selectTheme } from "@/features/settings/settingsSlice";
+import type { ParserDataItem } from "@/hooks/useParserData";
 
 type WeatherItem = {
     description: string;
@@ -38,113 +48,137 @@ type WeatherResponse = {
 
 function getWeatherVideoByIcon(icon: string) {
     switch (icon) {
-        case '01d':
+        case "01d":
             return {
                 vbb: vbb_clear,
                 solt: solt_clear,
-                lis: vbb_clear,
-                a78: solt_clear,
-                kpd: solt_clear
-            }
-        case '02d':
+                lis: lis_clear,
+                a78: lis_clear,
+                kpd: kpd_clear,
+            };
+        case "02d":
             return {
                 vbb: vbb_few_clouds,
                 solt: solt_few_clouds,
-                lis: vbb_few_clouds,
-                a78: solt_few_clouds,
-                kpd: solt_few_clouds
-            }
-        case '03d':
+                lis: lis_few_clouds,
+                a78: lis_few_clouds,
+                kpd: kpd_few_clouds,
+            };
+        case "03d":
             return {
                 vbb: vbb_cloudy,
                 solt: solt_cloudy,
-                lis: vbb_cloudy,
-                a78: solt_cloudy,
-                kpd: solt_cloudy
-            }
-        case '04d':
+                lis: lis_few_clouds,
+                a78: lis_few_clouds,
+                kpd: kpd_few_clouds,
+            };
+        case "04d":
             return {
                 vbb: vbb_cloudy,
                 solt: solt_cloudy,
-                lis: vbb_cloudy,
-                a78: solt_cloudy,
-                kpd: solt_cloudy
-            }
-        case '09d':
+                lis: lis_heavy_clouds,
+                a78: lis_heavy_clouds,
+                kpd: kpd_heavy_clouds,
+            };
+        case "09d":
             return {
                 vbb: vbb_rain,
                 solt: solt_rain,
-                lis: vbb_rain,
-                a78: solt_rain,
-                kpd: solt_rain
-            }
-        case '10d':
+                lis: lis_rain,
+                a78: lis_rain,
+                kpd: kpd_rain,
+            };
+        case "10d":
             return {
                 vbb: vbb_rain,
                 solt: solt_rain,
-                lis: vbb_rain,
-                a78: solt_rain,
-                kpd: solt_rain
-            }
-        case '11d':
+                lis: lis_rain,
+                a78: lis_rain,
+                kpd: kpd_rain,
+            };
+        case "11d":
             return {
                 vbb: vbb_thunder,
                 solt: solt_thunder,
-                lis: vbb_thunder,
-                a78: solt_thunder,
-                kpd: solt_thunder
-            }
+                lis: lis_thunder,
+                a78: lis_thunder,
+                kpd: kpd_heavy_rain,
+            };
         default:
             return {
                 vbb: vbb_clear,
                 solt: solt_clear,
                 lis: vbb_clear,
                 a78: solt_clear,
-                kpd: solt_clear
-            }
+                kpd: kpd_clear,
+            };
     }
 }
 
 function isWeatherData(data: unknown): data is WeatherResponse {
-    if (typeof data !== 'object' || data === null) return false;
+    if (typeof data !== "object" || data === null) return false;
 
     const maybeWeather = data as WeatherResponse;
 
     if (!Array.isArray(maybeWeather.weather)) return false;
     for (const item of maybeWeather.weather) {
-        if (typeof item?.description !== 'string' ||
-            typeof item?.icon !== 'string') {
+        if (
+            typeof item?.description !== "string" ||
+            typeof item?.icon !== "string"
+        ) {
             return false;
         }
     }
 
-    if (typeof maybeWeather.main !== 'object' || maybeWeather.main === null) return false;
-    if (typeof maybeWeather.main.temp !== 'number' ||
-        typeof maybeWeather.main.feels_like !== 'number' ||
-        typeof maybeWeather.main.pressure !== 'number' ||
-        typeof maybeWeather.main.humidity !== 'number') {
+    if (typeof maybeWeather.main !== "object" || maybeWeather.main === null)
+        return false;
+    if (
+        typeof maybeWeather.main.temp !== "number" ||
+        typeof maybeWeather.main.feels_like !== "number" ||
+        typeof maybeWeather.main.pressure !== "number" ||
+        typeof maybeWeather.main.humidity !== "number"
+    ) {
         return false;
     }
 
-    if (typeof maybeWeather.wind !== 'object' || maybeWeather.wind === null) return false;
-    if (typeof maybeWeather.wind.speed !== 'number') return false;
+    if (typeof maybeWeather.wind !== "object" || maybeWeather.wind === null)
+        return false;
+    if (typeof maybeWeather.wind.speed !== "number") return false;
 
     return true;
 }
 
 function WeatherCard(item: ParserDataItem) {
-    const org = useAppSelector(selectTheme)
-    const dispatch = useAppDispatch()
+    const org = useAppSelector(selectTheme);
+    const dispatch = useAppDispatch();
+    const mediaRef = useRef(null)
 
-    if (!org) return null
+    useEffect(() => {
+        // biome-ignore lint/complexity/useLiteralKeys: <don't care>
+        if (mediaRef.current && mediaRef.current["tagName"] === "IMG") {
+            const timeout = setTimeout(() => {
+                dispatch(setVideoSlideState("finished"))
+            }, 10000)
+
+            return () => clearTimeout(timeout)
+        }
+    }, [dispatch])
+
+    if (!org) return null;
     if (!isWeatherData(item.data)) {
-        return null
+        return null;
     }
 
-    const temperature = item.data.main.temp > 0 ? `+${Math.round(item.data.main.temp)}°` : `${Math.round(item.data.main.temp)}°`
-    const feelsLike = item.data.main.feels_like > 0 ? `+${Math.round(item.data.main.feels_like)}°` : `${Math.round(item.data.main.feels_like)}°`
+    const temperature =
+        item.data.main.temp > 0
+            ? `+${Math.round(item.data.main.temp)}°`
+            : `${Math.round(item.data.main.temp)}°`;
+    const feelsLike =
+        item.data.main.feels_like > 0
+            ? `+${Math.round(item.data.main.feels_like)}°`
+            : `${Math.round(item.data.main.feels_like)}°`;
 
-    const videoUrl = getWeatherVideoByIcon(item.data.weather[0].icon)
+    const videoUrl = getWeatherVideoByIcon(item.data.weather[0].icon);
 
     return (
         <div className="flex flex-col gap-4 max-w-4xl animate-rotate-y">
@@ -157,23 +191,35 @@ function WeatherCard(item: ParserDataItem) {
                     </div>
                     <div className="space-y-3">
                         <div className="text-3xl">Ощущается как {feelsLike}</div>
-                        <div className="text-3xl">{item.data.main.pressure * 0.75} мм. рт. столба</div>
-                        <div className="text-3xl">{item.data.wind.speed.toFixed(1)} м/сек</div>
+                        <div className="text-3xl">
+                            {item.data.main.pressure * 0.75} мм. рт. столба
+                        </div>
+                        <div className="text-3xl">
+                            {item.data.wind.speed.toFixed(1)} м/сек
+                        </div>
                         <div className="text-3xl">{item.data.main.humidity}% влажности</div>
                     </div>
                 </div>
-                <video
-                    className="absolute inset-0 rounded-xl z-10"
-                    onLoadStart={() => dispatch(setVideoSlideState("started"))}
-                    onEnded={() => dispatch(setVideoSlideState("finished"))}
-                    src={videoUrl[org]}
-                    preload="auto"
-                    autoPlay
-                    muted
-                />
+                {videoUrl[org].endsWith(".jpg") ?
+                    <img ref={mediaRef}
+                        className="absolute inset-0 rounded-xl z-10"
+                        src={videoUrl[org]}
+                        alt="weather preview"
+                    />
+                    :
+                    <video
+                        className="absolute inset-0 rounded-xl z-10"
+                        onLoadStart={() => dispatch(setVideoSlideState("started"))}
+                        onEnded={() => dispatch(setVideoSlideState("finished"))}
+                        src={videoUrl[org]}
+                        preload="auto"
+                        autoPlay
+                        muted
+                    />
+                }
             </div>
         </div>
-    )
+    );
 }
 
-export default WeatherCard
+export default WeatherCard;
